7e3ef3b0dc8ea104cd3495cd2d1bd47f83347af3
Aurélien Desbrières <aurelien@hackers.camp>
2016-03-22 05:45:43 +0100

    update and upgrade script

--- update.sh
+++ update.sh
@@ -5,20 +5,29 @@
 # License: GPL v2 or later
 # Author: Aurelien DESBRIERES - aurelien@hackers.camp
 
+
+# Script to update an upgrade on the remote machine
+
 command_exists () {
     type "$1" &> /dev/null ;
 }
 
-# For Debian / Ubuntu / Trisquel / gNewSense and other in the branch
+# For Debian / Ubuntu / Trisquel / gNewSense and derivatives
 if command_exists apt-get ; then
     sudo apt-get update && sudo apt-get upgrade
 fi
+
+# For Archlinux / Parabola and derivatives
 if command_exists pacman ; then
     sudo pacman -Syu
 fi
+
+# For Android / Cyanogen / Replicant and derivatives
 if command_exists apt ; then
     sudo apt update ; sudo apt upgrade
 fi
+
+# For RedHat / Fedora / Centos and derivatives
 if command_exists dnf ; then
     sudo dnf update
 else
