a125278c13a7f416c1a6512793717eda485d5fa9
Aurélien Desbrières <aurelien@hackers.camp>
2016-03-29 21:22:36 +0200

    Working 0.1 version

--- operator.sh
+++ operator.sh
@@ -9,4 +9,7 @@
 
 # Script to update an upgrade on the remote machine
 
-scp /root/mas-log*.tar.gz operator@operator:~/ ; exit
+user_directory=`ls /home > file ; cat file | grep -v lost+found`
+
+
+scp /home/$user_directory/mas-log*.tar.gz operator@operator:~/ ; exit
