04c3487b0236b2c592e3f98a1db530ecfae363fe
Aurélien Desbrières <aurelien@hackers.camp>
2016-03-31 21:39:51 +0200

    MAS 0.2 version + code verified with shellcheck

--- virusscan.sh
+++ virusscan.sh
@@ -8,15 +8,15 @@
 
 tput clear # clear the terminal
 
-printf "\n\033[1;32mWelcome to FARON - Forensic Analyser Remote Over Network\033[0m\n"
-printf "\033[1;32mFARON is made to run as a MAS dependencie\033[0m\n"
-printf "\033[1;32mFARON will run during more than one hour!\033[0m\n"
+printf "\033[1;32mWelcome to FARON - Forensic Analyser Remote Over Network\033[0m%s\n"
+printf "\033[1;32mFARON is made to run as a MAS dependencie\033[0m%s\n"
+printf "\033[1;32mFARON will run during more than one hour!\033[0m%s\n"
 
 
 # Register the user identification during process
-printf "\033[1;32m\nRegistering you identification during the MAS process\033[0m\n"
+printf "\033[1;32m%s\nRegistering you identification during the MAS process\033[0m%s\n"
 if [ ! -S ~/.ssh/ssh_auth_sock ]; then
-    eval `ssh-agent`
+    eval "$(ssh-agent)"
     ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
 fi
 export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
@@ -25,7 +25,7 @@ ssh-add -l | grep "The agent has no identities" && ssh-add
 
 
 # Vefify and install the dependencies if needed
-printf "\nThe software will now get the needed dependencies for your\noperating system $the_user\n"  
+printf "%s\nThe software will now get the needed dependencies for your\noperating system $the_user%s\n"  
 
 command_exists () {
     type "$1" &> /dev/null ;
@@ -56,36 +56,30 @@ fi
 
 
 # command
-the_user=`whoami`
-the_machine=`hostname`
-network=`ip addr show | awk '/inet.*brd/{print $NF; exit}'`
-ip_only=`ip addr show $network | grep "inet\b" | awk '{print $2}' | cut -d/ -f1`
-
-sudo mkdir MAS-REPORT
+the_user="$(whoami)"
+#the_machine="$(hostname)"
+#network="$(ip addr show | awk '/inet.*brd/{print $NF; exit}')"
+#ip_only="$(ip addr show $network | grep 'inet\b' | awk '{print $2}' | cut -d/ -f1)"
+ip_only="$(awk -F'[ /]+'  '/inet /{print $3}')"
+firewall="$(sudo iptables -L)"
+virusscan="$(sudo freshclam && sudo clamscan -ri --log=clam-log --cross-fs=yes /)"
+sudo mkdir -p MAS-REPORT
 
 ## Virus analysis
 ### scan all type of filesystem
 
-file_exist () {
-    type "$1" &> /dev/null ;
-}
+touch MAS-REPORT/faron-report-"$ip_only"-virus || exit
+truncate -s 0 MAS-REPORT/faron-report-"$ip_only"-virus
 
-if file_exist MAS-REPORT/faron-report-$ip_only-virus ; then
-    truncate -s 0 MAS-REPORT/faron-report-$ip_only-virus ; break
-else
-    touch MAS-REPORT/faron-report-$ip_only-virus
-fi
-
-printf "\n\033[1;32mYour firewall rules are:\033[0m\n$firewall\n" | tee -a MAS-REPORT/faron-report-$ip_only-virus
+printf "%s\n\033[1;32mYour firewall rules are:\033[0m\n$firewall%s\n" | tee -a MAS-REPORT/faron-report-"$ip_only"-virus
 
 # request what the admin want
-printf "\n\033[1;32mScanning against virus will take more than one hour\033[0m\n"
-virusscan=`sudo freshclam && sudo clamscan -ri --log=clam-log --cross-fs=yes /`
-printf "\033[1;32mResult of the virus scan:\033[0m\n$virusscan\n" | tee -a MAS-REPORT/faron-report-$ip_only-virus
-
+printf "%s\n\033[1;32mScanning against virus will take more than one hour\033[0m%s\n"
+"$virusscan"
+printf "\033[1;32mResult of the virus scan:\033[0m\n$virusscan%s\n" | tee -a MAS-REPORT/faron-report-"$ip_only"-virus
+t
 
 ## Bring back all log to the admin
-user_directory=`ls /home > file ; cat file | grep -v lost+found`
-mkdir ~/MAS-REPORT/LOG/
-sudo tar czvf mas-virus-log$ip_only.tar.gz ~/MAS-REPORT/LOG/
+mkdir -p ~/MAS-REPORT/LOG/
+sudo tar czvf mas-virus-log"$ip_only".tar.gz ~/MAS-REPORT/LOG/
 
